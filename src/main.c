93a7406795d3f35508c590eadbfbb6c46179688b
Daniel Tar <tardani95@gmail.com>
2018-04-24 12:46:10 +0200

    ledstrip test - 800[kHz] - full 1 bit

--- src/main.c
+++ src/main.c
@@ -16,6 +16,8 @@ Info        : 2018-04-09
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"  /* reset and clock control */
 
+#include "stm32f10x_tim.h"  /* timer header */
+
 #include "stm32f10x_exti.h" /* external interrupt*/
 #include "stm32f1xx_it.h" 	/* interrupt handler */
 
@@ -63,9 +65,8 @@ int main(void)
 	GPIO_InitTypeDef GPIO_InitStructure;
 	EXTI_InitTypeDef EXTI_InitStructure; /* external interrupt init */
 	NVIC_InitTypeDef NVIC_InitStructure; /* nested vector interrupt controller init */
-
-//	TIM_TimeBaseInitTypeDef TIM_TimeBase_InitStructure; // timer init
-//	TIM_OCInitTypeDef TIM_OC_InitStructure; // output compare init
+	TIM_TimeBaseInitTypeDef TIM_TimeBase_InitStructure; // timer init
+	TIM_OCInitTypeDef TIM_OC_InitStructure; // output compare init
 
 
 	/* enable the clock on the advanced peripheral buses - see the datasheet on page 11 */
@@ -73,8 +74,8 @@ int main(void)
 	 * pb0  - pwm output
 	 * pb13 - button input
 	 */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_TIM1 | RCC_APB2Periph_AFIO, ENABLE);
-	//RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 | RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
 	/* led && c14 && c15 gpio init */
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
@@ -83,10 +84,10 @@ int main(void)
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 	/* pwm gpio init*/
-//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	/* button init */
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
@@ -112,105 +113,56 @@ int main(void)
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
 	NVIC_Init(&NVIC_InitStructure);
 
-	/*timer init*/
-//		TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-//		TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-//		TIM_TimeBase_InitStructure.TIM_Period = 999;
-//		TIM_TimeBase_InitStructure.TIM_Prescaler = 71;
-//		TIM_TimeBaseInit(TIM1, &TIM_TimeBase_InitStructure);
-//
-//		TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-//		TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-//		TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-//		TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//		TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-//		TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//		TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-//		TIM_OC_InitStructure.TIM_Pulse = 100;
-//
-//		TIM_CtrlPWMOutputs(TIM1, ENABLE);
-//		TIM_OC2Init(TIM1, &TIM_OC_InitStructure);
-//		TIM_Cmd(TIM1, ENABLE);
-//
-//
-//		//timer2 ch2
-//		TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-//		TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-//		TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-//		TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//		TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-//		TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//		TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-//		TIM_OC_InitStructure.TIM_Pulse = 500;
-//
-//		TIM_OC2Init(TIM2, &TIM_OC_InitStructure);
-//
-//		TIM_Cmd(TIM2, ENABLE);
-//
-//		//timer3 ch1
-//		TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-//		TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-//		TIM_TimeBase_InitStructure.TIM_Period = 1499;
-//		TIM_TimeBase_InitStructure.TIM_Prescaler = 71;
-//		TIM_TimeBaseInit(TIM3, &TIM_TimeBase_InitStructure);
-//
-//		TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-//		TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-//		TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-//		TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//		TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-//		TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//		TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-//		TIM_OC_InitStructure.TIM_Pulse = 100;
-//		TIM_OC1Init(TIM3, &TIM_OC_InitStructure);
-//
-//		//timer3 ch2
-//		TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-//		TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-//		TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-//		TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//		TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-//		TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//		TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-//		TIM_OC_InitStructure.TIM_Pulse = 500;
-//		TIM_OC2Init(TIM3, &TIM_OC_InitStructure);
-//
-//		TIM_Cmd(TIM3, ENABLE);
-//
-//		//timer4 ch1
-//		TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-//		TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-//		TIM_TimeBase_InitStructure.TIM_Period = 4999;
-//		TIM_TimeBase_InitStructure.TIM_Prescaler = 71;
-//		TIM_TimeBaseInit(TIM4, &TIM_TimeBase_InitStructure);
-//
-//		TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-//		TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-//		TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-//		TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//		TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-//		TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//		TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-//		TIM_OC_InitStructure.TIM_Pulse = 100;
-//		TIM_OC1Init(TIM4, &TIM_OC_InitStructure);
-//
-//		//timer4 ch2
-//		TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-//		TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-//		TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-//		TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-//		TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-//		TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
-//		TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-//		TIM_OC_InitStructure.TIM_Pulse = 500;
-//		TIM_OC2Init(TIM4, &TIM_OC_InitStructure);
-//
-//		TIM_Cmd(TIM4, ENABLE);
+	/*timer3 ch3 init*/
+	TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBase_InitStructure.TIM_Period = 9;
+	TIM_TimeBase_InitStructure.TIM_Prescaler = 8;
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBase_InitStructure);
+
+	TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+	TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OC_InitStructure.TIM_Pulse = 2;
+
+	TIM_OC3Init(TIM3, &TIM_OC_InitStructure);
+	TIM_Cmd(TIM3, ENABLE);
 
 
+//	//timer2 ch2
+//	TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+//	TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+//	TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
+//	TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+//	TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
+//	TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
+//	TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+//	TIM_OC_InitStructure.TIM_Pulse = 500;
+//
+//	TIM_OC2Init(TIM2, &TIM_OC_InitStructure);
+//
+//	TIM_Cmd(TIM2, ENABLE);
+//
+//	//timer3 ch1
+//	TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+//	TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+//	TIM_TimeBase_InitStructure.TIM_Period = 1499;
+//	TIM_TimeBase_InitStructure.TIM_Prescaler = 71;
+//	TIM_TimeBaseInit(TIM3, &TIM_TimeBase_InitStructure);
+//
+//	TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+//	TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+//	TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
+//	TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+//	TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
+//	TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
+//	TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+//	TIM_OC_InitStructure.TIM_Pulse = 100;
+//	TIM_OC1Init(TIM3, &TIM_OC_InitStructure);
 
 	/* variable to read output data register (ODR)*/
-	uint32_t readValue = 0;
+	//uint32_t readValue = 0;
 
 	/*switch off the led by default*/
 	GPIO_WriteBit(GPIOC,led_pin,Bit_SET);
