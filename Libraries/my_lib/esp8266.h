dbd8293b6cc1bea4423f8f06946e17a6a3875a6b
Daniel Tar <tardani95@gmail.com>
2018-06-15 23:46:02 +0200

    esp lib files updated

--- Libraries/my_lib/esp8266.h
+++ Libraries/my_lib/esp8266.h
@@ -1,9 +1,13 @@
 /*
- * esp8266.h
- *
- *  Created on: 31.05.2018
- *      Author: tar
- */
+******************************************************************************
+File        : esp8266.c
+Author      : Daniel Tar
+Version     :
+Copyright   :
+Description : esp8266 (ESP-01) library for STM32F10x
+Info        : 31.05.2018
+*******************************************************************************
+*/
 
 #ifndef LIBRARIES_MY_LIB_ESP8266_H_
 #define LIBRARIES_MY_LIB_ESP8266_H_
