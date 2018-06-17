5dcf24e8c5de9b0d8a015c0a512531327046735b
Daniel Tar <tardani95@gmail.com>
2018-06-17 19:45:39 +0200

    function declarations added

--- my_libs/ws2811.c
+++ my_libs/ws2811.c
@@ -12,7 +12,7 @@ Info        : 16.06.2018
 /******************************************************************************/
 /*                               Includes									  */
 /******************************************************************************/
-#include "../my_libs/ws2811.h"
+#include "ws2811.h"
 
 
 /******************************************************************************/
@@ -30,3 +30,17 @@ Info        : 16.06.2018
 /******************************************************************************/
 /*                          Function definitions                              */
 /******************************************************************************/
+void InitGPIO_LSSs(GPIO_InitTypeDef*);
+void InitNVIC_LSS(NVIC_InitTypeDef*);
+void InitDMA_CH3_TIM3_CHs(DMA_InitTypeDef*,uint8_t*);
+void InitTIM3_CLK(TIM_TimeBaseInitTypeDef*);
+void InitTIM3_PWM(TIM_OCInitTypeDef*);
+
+/* interval: [ fromCH ; toCh ) */
+void Init_WS2811(uint8_t* ptr_command_array, uint8_t command_array_size);
+
+uint8_t gammaCorrection(uint8_t color);
+void refreshLedStrip(void);
+
+/* dma buffer handling*/
+void DMA1_Channel3_IRQHandler(void);
--- my_libs/ws2811.h
+++ my_libs/ws2811.h
@@ -9,9 +9,74 @@ Info        : 16.06.2018
 *******************************************************************************
 */
 
-#ifndef LIBRARIES_MY_LIB_WS2811_H_
-#define LIBRARIES_MY_LIB_WS2811_H_
+#ifndef _WS2811_H_
+#define _WS2811_H_
 
+/*========================================================================*/
+/*                             INCLUDES									  */
+/*========================================================================*/
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"   /* reset and clock control */
+#include "stm32f10x_gpio.h"  /* general purpose input output */
+#include "stm32f10x_tim.h"   /* timer header */
+#include "stm32f10x_dma.h"   /* direct memory access */
 
+#include "stm32f10x_exti.h"  /* external interrupt */
+#include "stm32f1xx_it.h" 	 /* interrupt handler  */
 
-#endif /* LIBRARIES_MY_LIB_WS2811_H_ */
+/* High level functions for NVIC and SysTick (add-on to CMSIS functions)
+ * NVIC - Nested Vector Interrupt Controller
+ */
+#include "misc.h"
+
+#include "util.h" /* my lib - delay functions */
+/*========================================================================*/
+/*                           WS2811  SETTINGS							  */
+/*========================================================================*/
+
+/* Select operating mode according to your led strip */
+/*#define MODE_800kHz*/
+#define MODE_400kHz
+
+/* high period times in counter ticks @72MHz */
+/* 90 tick is equal with 1.25 us - prescaler = 0 */
+/* 90 tick is equal with 2.5  us - prescaler = 1 */
+#ifdef MODE_400kHz
+	#define TIM_PRESCALER 1
+#endif /*MODE_400kHz*/
+#ifdef MODE_800kHz
+	#define TIM_PRESCALER 0
+#endif /*MODE_800kHz*/
+
+/* according to ws2811 datasheet */
+#define TIM_PERIOD 90
+#define T1H 43
+#define T0H 18
+/*========================================================================*/
+
+#define LED_NUMBER_ON_ONE_STRIP 150
+#define LED_PER_IC              3
+#define DIFFERENT_LEDGROUPS (LED_NUMBER_ON_ONE_STRIP / LED_PER_IC)
+
+#define PARALELL_STRIPS 3 /* 1-4 */
+#define SERIES_STRIPS 1   /* 1-.. depends on power supply */
+#define PIXEL_ARRAY_SIZE ( PARALELL_STRIPS * (SERIES_STRIPS * DIFFERENT_LEDGROUPS * 3))
+
+/*========================================================================*/
+
+void InitGPIO_LSSs(GPIO_InitTypeDef*);
+void InitNVIC_LSS(NVIC_InitTypeDef*);
+void InitDMA_CH3_TIM3_CHs(DMA_InitTypeDef*,uint8_t*);
+void InitTIM3_CLK(TIM_TimeBaseInitTypeDef*);
+void InitTIM3_PWM(TIM_OCInitTypeDef*);
+
+/* interval: [ fromCH ; toCh ) */
+void Init_WS2811(uint8_t* ptr_command_array, uint8_t command_array_size);
+
+uint8_t gammaCorrection(uint8_t color);
+void refreshLedStrip(void);
+
+/* dma buffer handling*/
+void DMA1_Channel3_IRQHandler(void);
+
+#endif /* _WS2811_H_ */
