ac234a182c0b98b88f8e8673961903ecd0455862
Daniel Tar <tardani95@gmail.com>
2018-06-17 16:30:42 +0200

    util ilb added - delay functions

---
+++ my_libs/util.c
@@ -0,0 +1,68 @@
+/*
+******************************************************************************
+File        : util.c
+Author      : Peter Vass - modified and extended by Daniel Tar
+Version     :
+Copyright   :
+Description :
+Info        : 17.06.2018
+*******************************************************************************
+*/
+#include "util.h"
+
+
+/******************************************************************************/
+/*                          Private variables                                 */
+/******************************************************************************/
+__IO uint32_t SysTickCount=0; /* counts nr. of ticks since boot */
+
+
+
+/******************************************************************************/
+/*                          Function definitions                              */
+/******************************************************************************/
+/**
+  * @brief  increment SysTick counter, useful for delay functions
+  *         with systick f=1kHz, it overflows every 49 days
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void){
+	SysTickCount++;
+}
+
+/**
+  * @brief  increment SysTick counter, useful for delay functions
+  *         with systick f=1MHz, it overflows every 1.19 hours
+  * @param  None
+  * @retval None
+  */
+void InitSysTick(void){
+	RCC_ClocksTypeDef RCC_ClockStruct;
+	RCC_GetClocksFreq(&RCC_ClockStruct);
+
+	SysTick_Config(RCC_ClockStruct.SYSCLK_Frequency/SYS_TICK_FREQ-1); /* 1ms/SysClk tick */
+}
+
+void DelayUs(uint32_t us){
+	uint32_t startTick=SysTickCount;
+
+	/*overflow check*/
+	if(UINT32_MAX - startTick <= us ){
+		SysTickCount = startTick = 0;
+	}
+
+	while(SysTickCount-startTick <= us){}
+}
+
+void DelayMs(uint32_t ms){
+	for(uint32_t i = 0; i<ms; ++i){
+		DelayUs(1000); /*  the error grows with higher ms values*/
+	}
+}
+
+void DelaySec(uint32_t sec){
+	for(uint32_t i = 0; i<sec; ++i){
+		DelayMs(999); /*  the error grows with higher sec values*/
+	}
+}
---
+++ my_libs/util.h
@@ -0,0 +1,28 @@
+/*
+******************************************************************************
+File        : util.h
+Author      : Peter Vass - modified by Daniel Tar
+Version     :
+Copyright   :
+Description :
+Info        : 17.06.2018
+*******************************************************************************
+*/
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h" /* Reset & Clock Control */
+
+
+#define SYS_TICK_FREQ (1000000) /* frequency of sys_tick event in Hz - used by InitSystick() function */
+
+void SysTick_Handler(void);
+void InitSysTick(void);
+void DelayUs(uint32_t us);
+void DelayMs(uint32_t ms);
+void DelaySec(uint32_t sec);
+
+#endif /* _UTIL_H_ */
